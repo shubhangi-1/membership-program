8e32750e84965105005990796e1258bfedb191d4
shubhangi <46108881+shubhangi-1@users.noreply.github.com>
2026-06-23 19:27:21 +0530

    Update SubscribeRequest.java

--- src/main/java/com/firstclub/dto/SubscribeRequest.java
+++ src/main/java/com/firstclub/dto/SubscribeRequest.java
@@ -1,11 +1,17 @@
-package com.firstclub.dto; 
+package com.firstclub.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 @Data
 public class SubscribeRequest {
 
+    @NotNull(message = "UserId is required")
     private Long userId;
 
+    @NotNull(message = "PlanId is required")
     private Long planId;
 
+    @NotNull(message = "TierId is required")
     private Long tierId;
 }
