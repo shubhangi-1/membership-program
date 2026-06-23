93ad65a8b14434730e437f44016870561615e635
shubhangi <46108881+shubhangi-1@users.noreply.github.com>
2026-06-23 19:48:13 +0530

    Update MembershipUpgradedEvent.java

--- src/main/java/com/firstclub/events/MembershipUpgradedEvent.java
+++ src/main/java/com/firstclub/events/MembershipUpgradedEvent.java
@@ -1,1 +1,43 @@
-package com.firstclub.events; public class MembershipUpgradedEvent {}
+package com.firstclub.event;
+
+import com.firstclub.entity.Subscription;
+
+public class MembershipUpgradedEvent {
+
+    private final Long userId;
+    private final Long subscriptionId;
+    private final String oldTier;
+    private final String newTier;
+    private final String planName;
+
+    public MembershipUpgradedEvent(Subscription subscription,
+                                   String oldTier,
+                                   String newTier) {
+
+        this.userId = subscription.getUserId();
+        this.subscriptionId = subscription.getId();
+        this.planName = subscription.getPlan().getName();
+        this.oldTier = oldTier;
+        this.newTier = newTier;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public String getOldTier() {
+        return oldTier;
+    }
+
+    public String getNewTier() {
+        return newTier;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+}
