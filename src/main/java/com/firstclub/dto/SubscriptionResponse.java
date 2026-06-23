4bc4a1a7a06db4359735fc4b55ddd78568194a6b
shubhangi <46108881+shubhangi-1@users.noreply.github.com>
2026-06-23 19:27:49 +0530

    Update SubscriptionResponse.java

--- src/main/java/com/firstclub/dto/SubscriptionResponse.java
+++ src/main/java/com/firstclub/dto/SubscriptionResponse.java
@@ -1,1 +1,25 @@
+package com.firstclub.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionResponse {
+
+    private Long userId;
+
+    private String planName;
+
+    private String tierName;
+
+    private LocalDate startDate;
+
+    private LocalDate expiryDate;
+
+    private String status;
+}
