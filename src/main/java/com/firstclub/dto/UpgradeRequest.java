85b65a0fb6b861552f347263b300045a76dbbfdd
shubhangi <46108881+shubhangi-1@users.noreply.github.com>
2026-06-23 19:28:36 +0530

    Update UpgradeRequest.java

--- src/main/java/com/firstclub/dto/UpgradeRequest.java
+++ src/main/java/com/firstclub/dto/UpgradeRequest.java
@@ -1,1 +1,14 @@
-package com.firstclub.dto; public class UpgradeRequest {}
+package com.firstclub.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class UpgradeRequest {
+
+    @NotNull(message = "UserId is required")
+    private Long userId;
+
+    @NotNull(message = "TierId is required")
+    private Long tierId;
+}
