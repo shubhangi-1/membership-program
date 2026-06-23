c5a09f67a7ce7b87746f72df229b93dea9c91391
shubhangi <46108881+shubhangi-1@users.noreply.github.com>
2026-06-23 20:33:44 +0530

    Update ExpiryScheduler.java

--- src/main/java/com/firstclub/scheduler/ExpiryScheduler.java
+++ src/main/java/com/firstclub/scheduler/ExpiryScheduler.java
@@ -1,1 +1,29 @@
-package com.firstclub.scheduler; public class ExpiryScheduler {}
+package com.firstclub.scheduler;
+
+import com.firstclub.repository.SubscriptionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class ExpiryScheduler {
+
+    private final SubscriptionRepository subscriptionRepository;
+
+    /**
+     * Runs every day at midnight
+     * Expires all subscriptions whose expiryDate < today
+     */
+    @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
+    public void expireSubscriptions() {
+
+        int updatedRows = subscriptionRepository.expireOldSubscriptions();
+
+        System.out.println(
+                "ExpiryScheduler executed. Rows updated: " + updatedRows
+        );
+    }
+}
