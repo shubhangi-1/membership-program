22a562b30082687ab52bce163b7c5bd8715de1bf
shubhangi <46108881+shubhangi-1@users.noreply.github.com>
2026-06-23 19:46:04 +0530

    Update MembershipCreatedEvent.java

--- src/main/java/com/firstclub/events/MembershipCreatedEvent.java
+++ src/main/java/com/firstclub/events/MembershipCreatedEvent.java
@@ -1,1 +1,34 @@
-package com.firstclub.events; public class MembershipCreatedEvent {}
+package com.firstclub.event;
+
+import com.firstclub.entity.Subscription;
+
+public class MembershipCreatedEvent {
+
+    private final Long userId;
+    private final Long subscriptionId;
+    private final String planName;
+    private final String tierName;
+
+    public MembershipCreatedEvent(Subscription subscription) {
+        this.userId = subscription.getUserId();
+        this.subscriptionId = subscription.getId();
+        this.planName = subscription.getPlan().getName();
+        this.tierName = subscription.getTier().getTierName();
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+
+    public String getTierName() {
+        return tierName;
+    }
+}
