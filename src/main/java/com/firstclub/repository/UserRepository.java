89abe8089935dc12df4afa3d57988bc73f05ac2f
shubhangi <46108881+shubhangi-1@users.noreply.github.com>
2026-07-14 00:35:45 +0530

    Update UserRepository.java

--- src/main/java/com/firstclub/repository/UserRepository.java
+++ src/main/java/com/firstclub/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.firstclub.repository;
 import com.firstclub.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,4 +15,9 @@ public interface UserRepository
         extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+        @Lock(LockModeType.PESSIMISTIC_WRITE)
+        @Query("SELECT u FROM User u WHERE u.id = :id")
+        Optional<User> findByIdForUpdate(@Param("id") Long id);
+                
 }
